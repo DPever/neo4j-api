0c6148cfdf97430906eba7c1cd56b7bf5c2b828e
Daryl Pever <dpever@mre-consulting.com>
2026-01-19 15:04:10 -0600

    rework validations for OAC

--- src/validators/operationallyAvailableCapacityValidator.js
+++ src/validators/operationallyAvailableCapacityValidator.js
@@ -6,8 +6,8 @@ const enumSet = (values) => ({
   message: () => values.join(', ')
 });
 
-const DIR = enumSet(['', 'Backhaul', 'ForwardHaul']);
-const GROSS_NET = enumSet(['GROSS', 'NET']); 
+const DIR = enumSet(['', 'Delivery', 'Receipt', 'Bidirectional', 'Throughput']);
+const GROSS_NET = enumSet(['GROSS', 'NET']);
 const IT = enumSet(['Y', 'N', '']);
 const SCHED_STATUS = enumSet(['PRELIM', 'FINAL']);
 
@@ -16,7 +16,8 @@ export function validateOacRow(oac, idx = 0) {
 
   const add = (msg) => errs.push(`Row ${idx}: ${msg}`);
 
-  if (oac.direction && !DIR.set.has(oac.direction)) add(`Invalid direction '${oac.direction}'. Allowed: ${DIR.message()}`);
+  // suspend enforcement of direction for now
+  // if (oac.direction && !DIR.set.has(oac.direction)) add(`Invalid direction '${oac.direction}'. Allowed: ${DIR.message()}`);
   if (oac.grossOrNet && !GROSS_NET.set.has(oac.grossOrNet)) add(`Invalid grossOrNet '${oac.grossOrNet}'. Allowed: ${GROSS_NET.message()}`);
   if (oac.itIndicator && !IT.set.has(oac.itIndicator)) add(`Invalid itIndicator '${oac.itIndicator}'. Allowed: ${IT.message()}`);
   if (oac.schedStatus && !SCHED_STATUS.set.has(oac.schedStatus)) add(`Invalid schedStatus '${oac.schedStatus}'. Allowed: ${SCHED_STATUS.message()}`);
